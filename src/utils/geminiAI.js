aabc922686efa1d4d7f999d11f0f074656015f3a
Galih Aditya M <galihadityam1@gmail.com>
2024-04-18 17:57:14 +0700

    connection gemini AI done

---
+++ src/utils/geminiAI.js
@@ -0,0 +1,16 @@
+const { GoogleGenerativeAI } = require('@google/generative-ai')
+require('dotenv').config()
+
+console.log(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
+
+async function run() {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" })
+    const prompt = "Write a story about a magic backpack."
+    const result = await model.generateContent(prompt)
+    const response = await result.response
+    const text = response.text()
+
+    console.log(text);
+}
+run()
