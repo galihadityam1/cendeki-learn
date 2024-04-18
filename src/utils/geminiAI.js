ae7e10cba96340a5a294fdf9970e70b56e7d8911
Galih Aditya M <galihadityam1@gmail.com>
2024-04-18 18:36:07 +0700

    generate image using geminiAI done

--- src/utils/geminiAI.js
+++ src/utils/geminiAI.js
@@ -1,16 +1,31 @@
-const { GoogleGenerativeAI } = require('@google/generative-ai')
-require('dotenv').config()
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require("dotenv").config();
+const fs = require("fs");
 
-console.log(process.env.GEMINI_API_KEY);
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
+// console.log(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType,
+    },
+  };
+}
 
 async function run() {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" })
-    const prompt = "Write a story about a magic backpack."
-    const result = await model.generateContent(prompt)
-    const response = await result.response
-    const text = response.text()
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+  const prompt = "What's different between these pictures?";
+  const imageParts = [
+    fileToGenerativePart("src/utils/kucing.jpeg", "image/jpeg"),
+    fileToGenerativePart("src/utils/naruto.jpeg", "image/jpeg"),
+  ];
+  const result = await model.generateContent([prompt, ...imageParts]);
+  const response = await result.response;
+  const text = response.text();
 
-    console.log(text);
+  console.log(text);
 }
-run()
+
+run();
