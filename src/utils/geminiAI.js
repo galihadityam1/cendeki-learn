89b8706b7a3e472e2c95b48222a544119da9d03f
Galih Aditya M <galihadityam1@gmail.com>
2024-04-18 19:10:54 +0700

    chat in open ai done

--- src/utils/geminiAI.js
+++ src/utils/geminiAI.js
@@ -1,29 +1,27 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
-const fs = require("fs");
 
-// console.log(process.env.GEMINI_API_KEY);
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-function fileToGenerativePart(path, mimeType) {
-  return {
-    inlineData: {
-      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
-      mimeType,
-    },
-  };
-}
-
 async function run() {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
-  const prompt = "What's different between these pictures?";
-  const imageParts = [
-    fileToGenerativePart("src/utils/kucing.jpeg", "image/jpeg"),
-    fileToGenerativePart("src/utils/naruto.jpeg", "image/jpeg"),
-  ];
-  const result = await model.generateContent([prompt, ...imageParts]);
-  const response = await result.response;
-  const text = response.text();
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: "Hello. I have 2 dogs in my house.",
+      },
+      {
+        role: "model",
+        parts: "Great to meet you. What would you like to know?",
+      },
+    ],
+  });
+
+  const msg = "How many paws are in my house?";
+
+  const result = await chat.sendMessage(msg);
+  const text = result.response.text(); // gunakan result.response.text
 
   console.log(text);
 }
