97430ea16ea66d3c7f1283b3248d9d4194a808f5
Galih Aditya M <galihadityam1@gmail.com>
2024-04-19 11:39:24 +0700

    feat: getStoryById

--- src/db/models/storyModel.jsx
+++ src/db/models/storyModel.jsx
@@ -7,12 +7,16 @@ export class StoryModel {
         return getCollection("story")
     }
 
-    static async getStoryByUser() {
-        return await this.collection.find({_id})
+    static async getStoryByUser(_id) {
+        return await this.collection.find({userId : _id})
     }
     
     static async addStory(text) {
         const result = await this.collection().insertOne(text)
         return result
+    }
+
+    static async getStoryById(_id){
+        return await this.collection.find({_id})
     }
 }
