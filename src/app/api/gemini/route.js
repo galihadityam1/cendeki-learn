20f8c634799a9a2a2ba9ce5109af06699e7a3700
Galih Aditya M <galihadityam1@gmail.com>
2024-04-19 11:05:10 +0700

    export utils to api done

--- src/app/api/gemini/route.js
+++ src/app/api/gemini/route.js
@@ -1,17 +1,11 @@
 import { StoryModel } from "@/db/models/storyModel";
+import generateStory from "@/utils/geminiAI";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
-const { GoogleGenerativeAI } = require('@google/generative-ai')
-
-// console.log(process.env.GEMINI_API_KEY);
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 
 export async function POST() {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-  const prompt = "Write a story about a pokemon.";
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  const story = await StoryModel.addStory({ text });
-  return NextResponse.json({data: text}, {status: 201});
+  const prompt = "Write a story about History in one pargaraf.";
+  const result = await generateStory(prompt);
+  const story = await StoryModel.addStory({ result });
+  return NextResponse.json({ data: result }, { status: 201 });
 }
